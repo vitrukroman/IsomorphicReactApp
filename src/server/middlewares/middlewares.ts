5c8bd6d001c698e3d7edc700db77acf02a46819b
Roman Vitruk <vitrukrom@gmail.com>
2018-03-23 17:21:31 +0200

    added favicon

--- src/server/middlewares/middlewares.ts
+++ src/server/middlewares/middlewares.ts
@@ -3,12 +3,13 @@ import express, { Application } from "express";
 import path from "path";
 import favicon from "serve-favicon";
 import { IConfig } from "../../config/createConfig";
+import "../../favicon.ico";
 
 const proxy = require("express-http-proxy");
 
 export default (app: Application, config: IConfig) => {
   app.use(compression());
-  app.use(favicon(path.resolve("src", "favicon.ico")));
+  app.use(favicon(path.resolve("dist", "favicon.ico")));
   app.use("/api", proxy(`${config.api.host}:${config.api.port}`));
 
   app.use(express.static("public"));
