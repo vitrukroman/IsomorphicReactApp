ea6114bae8117222225bfa04df175c9538e9c8e0
Roman Vitruk <vitrukrom@gmail.com>
2018-04-12 11:24:18 +0300

    Implemented asset hashing and new module to extract css

--- src/server/renderer.tsx
+++ src/server/renderer.tsx
@@ -1,15 +1,24 @@
-import {Request} from "express";
+import { Request } from "express";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-import {Helmet} from "react-helmet";
-import {Provider} from "react-redux";
-import {StaticRouter} from "react-router-dom";
-import {Store} from "redux";
+import { Helmet } from "react-helmet";
+import { Provider } from "react-redux";
+import { StaticRouter } from "react-router-dom";
+import { Store } from "redux";
 import serialize from "serialize-javascript";
 import App from "../components/app/app";
 import IContext from "../types/context";
 import IStore from "../types/store";
+
 const manifest = require("../../public/manifest.json")
+
+const bundleJs = process.env.NODE_ENV === "production"
+  ? manifest["bundle.js"]
+  : "bundle.js";
+
+const bundleCss = process.env.NODE_ENV === "production"
+  ? manifest["bundle.cs"]
+  : "bundle.js";
 
 export default (req: Request, store: Store<IStore>, context: IContext) => {
   const content = ReactDOMServer.renderToString(
@@ -29,7 +38,7 @@ export default (req: Request, store: Store<IStore>, context: IContext) => {
   <html>
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="${manifest["bundle.css"]}" >
+      <link rel="stylesheet" href="${bundleCss}" >
       ${helmet.title.toString()}
       ${helmet.meta.toString()}
     </head>
@@ -38,7 +47,7 @@ export default (req: Request, store: Store<IStore>, context: IContext) => {
       <script>
         window.__INITIAL_STATE__ = ${serialize(store.getState())}
       </script>
-      <script src="${manifest["bundle.js"]}">
+      <script src="${bundleJs}">
       </script>
     </body>
   </html>
